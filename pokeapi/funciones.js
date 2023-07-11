48f3b8b3c2b04dc6617c4d7438a57e32c7ea090e
juandivg <juanvgutt@gmail.com>
2023-07-11 13:35:54 -0500

    Creacion simulacro

--- pokeapi/funciones.js
+++ pokeapi/funciones.js
@@ -1,7 +1,7 @@
 import {container} from "./selectores.js"
-
+container.innerHTML="";
  export async function obetenerPokes(){
-    const url="https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+    const url="https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
     try {
         let response = await fetch(url)
         let dato=await response.json()
@@ -19,9 +19,16 @@ async function getPokemon(par){
             let dato= await fetch(element.url)
             let result= await dato.json();
             console.log(result);
+            renderPokes(result)
         });
     } 
     catch (error) {
         console.log(error)
     }
+}
+async function renderPokes(par){
+    const url=par.sprites.front_default
+    container.innerHTML+=`<div class="flex flex-col bg-yellow-400 justify-center items-center border border-black rounded-md">
+                            <h1 class="text-[30px] font-bold">${par.name}</h1>
+                            <img class="w-[150px] h-[150px]" src=${url}></div>`;
 }
