62251adefb59b3859258ec9ede4fb8dcf941f0cb
juandivg <juanvgutt@gmail.com>
2023-07-17 02:44:12 -0500

    cambios simulacro

--- simulacro/funciones.js
+++ simulacro/funciones.js
@@ -1,24 +1,29 @@
-import {inp, btn, container} from './selectores.js'
-container.innerHTML=""
+import {inp1, inp2, btn, btn2, container} from './selectores.js'
+
 export async function mostrarMeals(){
     const url="https://www.themealdb.com/api/json/v1/1/categories.php"
     try {
         let response= await fetch(url)
         let dato=await response.json();
-        renderMeals(dato);
+        renderCategories(dato);
     } catch (error) {
         console.log(error)
     }
 }
-function renderMeals(par){
+function renderCategories(par){
     console.log(par)
+    container.innerHTML=""
     par.categories.forEach(element => {
         container.innerHTML+=`<div class="flex flex-col border border-black rounded-md p-[10px]">
           <div class="flex">
           <p class="pr-[5px]">${element.idCategory}.</p>
           <p>${element.strCategory}</p>
           </div>
-          <img src=${element.strCategoryThumb}>                  
+          <img src=${element.strCategoryThumb}> 
+          <button id="btn2" onclick="check(${element.strCategory})" class="bg-red-400 text-white px-[10px] py-[5px] border rounded-md">Check meals</button>                 
           </div>`
     });
 } 
+function check(par){
+    
+}
--- simulacro/selectores.js
+++ simulacro/selectores.js
@@ -1,4 +1,6 @@
 let btn=document.querySelector('#button')
-let inp=document.querySelector('#category')
+let btn2=document.querySelector('#btn2')
+let inp1=document.querySelector('#area')
+let inp2=document.querySelector('#name')
 let container=document.querySelector('#container')
-export {btn, inp, container}
+export {btn, btn2, inp1, inp2, container}
